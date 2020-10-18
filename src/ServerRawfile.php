3c77e194c3cc8ff7fdb1156cc9e760eb2786b321
Halfhumun <austinjung929@gmail.com>
2020-10-18 19:55:29 +0900

    ServerRawfile.php (Web HTML Directorty)

--- src/ServerRawfile.php
+++ src/ServerRawfile.php
@@ -5,6 +5,8 @@
 		$lines = @file("source.txt") or $result = "파일을 읽을 수 없습니다.";
 		if ($lines != null){
 			for($i = 0;$i < count($lines);$i++){
+				$lines[$i] = str_replace("<","&lt",$lines[$i]);
+				$lines[$i] = str_replace("<","&gt",$lines[$i]);
 				$result .= $lines[$i] . "<br>";
             }
         }
